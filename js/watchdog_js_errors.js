abd19195e0592aab029a876b11241699c1d8b643
guilherme lopes <guiajlopes@gmail.com>
2014-08-26 00:00:31 -0300

    Initial structure of the module

---
+++ js/watchdog_js_errors.js
@@ -0,0 +1,32 @@
+(function($) {
+  Drupal.behaviors.watchdog_js_errors = {
+    attach: function(context, settings) {
+      $(window).bind('beforeunload', function() {
+        if (errors.length > 0) {
+          $.ajax({
+            url: "watchdog_js_errors",
+            data: {
+              errors: errors,
+            },
+            type: "POST",
+            dataType: 'json',
+            success: function(responce) {
+              console.log(responce);
+              errors = [];
+            }
+          });
+          return Drupal.t('Do you really want to close the window?');
+        }
+      });
+    }
+  };
+})(jQuery);
+
+// Save all errors in this variable.
+var errors = [];
+
+// Error event.
+window.onerror = function(msg, url, line, col, error) {
+  // Push errors in Array.
+  errors.push({message: msg, url: url, line: line});
+}
